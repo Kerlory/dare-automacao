2f3e6f4c9280691307db94ff92ddce67b89b3334
Kerlon <ryankerlon793@gmail.com>
2026-03-27 22:50:20 -0400

    fix selenium render

--- src/main/java/com/dare/Main.java
+++ src/main/java/com/dare/Main.java
@@ -29,13 +29,14 @@ public class Main {
             // 🔥 DRIVER AUTOMÁTICO
             WebDriverManager.chromedriver().setup();
 
+            WebDriverManager.chromedriver().setup();
+
             ChromeOptions options = new ChromeOptions();
             options.addArguments("--headless=new");
             options.addArguments("--no-sandbox");
             options.addArguments("--disable-dev-shm-usage");
             options.addArguments("--disable-gpu");
             options.addArguments("--remote-allow-origins=*");
-            options.addArguments("--window-size=1920,1080");
 
             WebDriver driver = new ChromeDriver(options);
             WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(15));
